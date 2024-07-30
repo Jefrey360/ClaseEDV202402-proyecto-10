0a0734f0bd3683fdc83eb979553f7fc1541a28c5
Jefrey360 <stivenpeter0@gmail.com>
2024-07-30 17:39:57 -0600

    Add files via upload

---
+++ Personalizado.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ClinicaMedica
+{
+    partial class Personalizado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            checkedListBox1 = new CheckedListBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Blue;
+            button1.Location = new Point(579, 420);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Location = new Point(264, 420);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 1;
+            button2.Text = "Eliminar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(52, 280);
+            label1.Name = "label1";
+            label1.Size = new Size(162, 25);
+            label1.TabIndex = 4;
+            label1.Text = "Ubicacion de Hotel";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(444, 166);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 25);
+            label2.TabIndex = 6;
+            label2.Text = "Habitaciones";
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = Color.Teal;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Hotel Santiago", "Hotel Esperanza", "Hotel Libertad" });
+            comboBox1.Location = new Point(52, 320);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(258, 33);
+            comboBox1.TabIndex = 7;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.BackColor = Color.Teal;
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "Habitacion Sencilla", "Habitacion Doble", "Habitacion Cuadruple" });
+            checkedListBox1.Location = new Point(444, 203);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(222, 88);
+            checkedListBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(49, 35);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 25);
+            label3.TabIndex = 10;
+            label3.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Teal;
+            textBox1.Location = new Point(49, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(261, 31);
+            textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Teal;
+            textBox2.Location = new Point(444, 75);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(279, 31);
+            textBox2.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(444, 35);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 25);
+            label4.TabIndex = 13;
+            label4.Text = "Correo";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(48, 150);
+            label5.Name = "label5";
+            label5.Size = new Size(79, 25);
+            label5.TabIndex = 14;
+            label5.Text = "Telefono";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.Teal;
+            textBox3.Location = new Point(52, 190);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(258, 31);
+            textBox3.TabIndex = 15;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(425, 420);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 16;
+            button3.Text = "Editar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Personalizado
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 192, 192);
+            ClientSize = new Size(783, 487);
+            Controls.Add(button3);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(checkedListBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            ForeColor = SystemColors.ControlText;
+            Name = "Personalizado";
+            Text = "Registro";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private CheckedListBox checkedListBox1;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox3;
+        private Button button3;
+    }
+}
---
+++ UserControl1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ClinicaMedica
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(429, 425);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "Aceptar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(256, 425);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 2;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(67, 425);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 3;
+            button3.Text = "Borrar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(67, 117);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(264, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(67, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 25);
+            label2.TabIndex = 5;
+            label2.Text = "Id";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(67, 215);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(264, 31);
+            textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(67, 178);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 25);
+            label3.TabIndex = 7;
+            label3.Text = "Nombre";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Soltero", "Casado" });
+            comboBox1.Location = new Point(67, 325);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(264, 33);
+            comboBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(77, 286);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Estado";
+            // 
+            // UserControl1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label4);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "UserControl1";
+            Size = new Size(619, 485);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label4;
+    }
+}
